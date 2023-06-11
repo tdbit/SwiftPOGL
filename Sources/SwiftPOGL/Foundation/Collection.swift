b85088719880b0a5d11593fb53251953a8a7e195
Tom Drummond <tom@updtd.com>
2023-06-10 20:26:42 -0700

    Combine random methods on Foundation Collections

--- Sources/SwiftPOGL/Foundation/Collection.swift
+++ Sources/SwiftPOGL/Foundation/Collection.swift
@@ -24,3 +24,48 @@ extension Collection {
         }
     }
 }
+
+
+extension Collection where Element: Equatable {
+
+    /// Returns a tuple containing two distinct random elements from the collection.
+    /// Will return nil if the collection has less than two distinct elements.
+    ///
+    /// Elements must conform to ``Equatable``
+    ///
+    /// - SeeAlso: ``randomPair()``
+    public func randomDistinctPair() -> (Element, Element)? {
+        switch count {
+        case 0, 1:
+            return nil
+
+        default:
+            let array = shuffled()
+            let u = array[0]
+            for v in array[1...] {
+                if (v != u) { return (u,v) }
+            }
+            return nil
+        }
+    }
+
+
+    /// Returns a `count`-sized array of pairs (i.e. 2-tuples) containing strictly distinct
+    /// random elements from the collection.
+    ///
+    /// The elements in each pair are guaranteed to be distinct even if the array contains
+    /// repeated values.  Each pair is not guaranteed to be distinct from all the other
+    /// pairs in the array.  Will return nil if the source array has less than two distinct
+    /// elements.  Elements must conform to ``Equatable``
+    ///
+    /// - SeeAlso: ``randomDistinctPair()``
+    public func randomDistinctPairs(count n: Int? = nil) -> [(Element, Element)] {
+        let pairs = n ?? count * (count - 1) / 2
+
+        var array:[(Element, Element)] = []
+        while let uv = randomDistinctPair(), array.count < pairs {
+            array.append(uv)
+        }
+        return array
+    }
+}
--- Sources/SwiftPOGL/Foundation/Set.swift
+++ Sources/SwiftPOGL/Foundation/Set.swift
@@ -1,5 +1,5 @@
 //
-//  Collection.swift
+//  Set.swift
 //  SwiftPOGL
 //
 //  Created by Tom Drummond on 6/5/23.
@@ -10,41 +10,22 @@ import Foundation
 
 extension Set {
 
-    /// Returns a tuple containing two distinct random elements from the set.
-    /// Will return nil if the set has less than two elements.
-    ///
-    /// - SeeAlso: ``randomPair()``
-    public func randomDistinctPair() -> (Element, Element)? {
-        guard count > 1 else {
-            return nil
-        }
-
-        let u = randomElement()
-        var v = randomElement()
-        while u == v { v = randomElement() }
-
-        return (u!,v!)
-    }
-    
     /// Returns a random subset of elements in the set.
     ///
-    /// If `count` is greater than the size of the set will return the whole set.
-    /// If `count` is nil the size of the subset will be random.  If count is negative
-    /// will return an empty set.
+    /// If `count` is nil the size of the subset will be random from 0 to the size of
+    /// the original set. If `count` is greater than the size of the original set it
+    /// will return the whole set.  If count is negative will return an empty set.
     public func randomSubset(count n: Int?) -> Self {
-        guard let n = n else {
-            let n = Int.random(in: 0...count)
-            return randomSubset(count: n)
+        let sizeOfSubset = n ?? Int.random(in: 0...count)
+
+        guard sizeOfSubset < count else {
+            return Self(self)
         }
 
-        if (n < count) {
-            var subset: Self = []
-            while subset.count < n {
-                subset.insert(randomElement()!)
-            }
-            return subset
+        var set = Self()
+        while set.count < sizeOfSubset {
+            set.insert(randomElement()!)
         }
-
-        return Self(self)
+        return set
     }
 }
