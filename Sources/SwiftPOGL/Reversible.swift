49ab7e4d2fa37ebc4548470c27d4c0e15a749bc3
Tom Drummond <tom@updtd.com>
2023-06-11 21:55:27 -0700

    Fix comment

--- Sources/SwiftPOGL/Reversible.swift
+++ Sources/SwiftPOGL/Reversible.swift
@@ -12,7 +12,7 @@ import Foundation
 /// type and the definition of what a reversed version is will vary by context.  In a graph
 /// theory context a directed edge is `Reversible` and the reverse of the edge is a copy of
 /// it with the order of its nodes swapped. Similarly a digraph is `Reversible` with the reverse
-/// being a copy of the digraph with the order of its nodes swapped.
+/// being a copy of the digraph with all its edges reversed.
 public protocol Reversible {
 
     /// Returns the reverse of the type.  What it means to reverse a type will vary by context.
