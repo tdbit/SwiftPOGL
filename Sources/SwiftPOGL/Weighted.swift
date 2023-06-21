6512d6ab68fc6497dd237f06e2dbb598adbfd8c6
Tom Drummond <tom@updtd.com>
2023-06-20 22:26:13 -0700

    Add Comparable protocol to Weighable typealias

--- Sources/SwiftPOGL/Weighted.swift
+++ Sources/SwiftPOGL/Weighted.swift
@@ -7,28 +7,58 @@
 
 import Foundation
 
-/// `Weighable` types are both `Hashable` and `Numeric`.
+/// `Weighable` types are `Numeric`, `Comparable`, and `Hashable`.
 ///
-/// Swift's ``Numeric``types are all ``Hashable`` but their conformance is defined at the
-/// struct level.  We define a `Weighable` typealias that is both `Hashable` and `Numeric`
-/// to help with autogeneration of `Hashable` methods for types that conform to the
-/// ``Weighted`` protocol.
-public typealias Weighable = Hashable & Numeric
+/// Swift's `Numeric`types are all `Hashable` but their conformance is defined at the
+/// struct level.  We define a `Weighable` typealias that is `Numeric`, `Comparable`,
+/// and `Hashable` to help with autogeneration of `Hashable` methods for types that
+/// conform to the `Weighted` protocol.
+public typealias Weighable = Numeric & Comparable & Hashable
 
 /// All `Weighted` types provide a typed numeric weight.
 public protocol Weighted {
 
-    associatedtype Weight: Hashable & Numeric
+    associatedtype Weight: Weighable
 
     /// The type's read-only numeric weight
     var weight: Weight { get }
 }
 
 
-// MARK: Simple type conformance
+// MARK: Default Implementations
 
-extension Double: Weighted {
+extension Numeric where Self: Weighted {
     public typealias Weight = Self
-
     public var weight: Self { self }
 }
+
+
+extension Equatable where Self: Edge, Self: Weighted {
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        (lhs.u == rhs.u) &&
+        (lhs.v == rhs.v) &&
+        (lhs.weight == rhs.weight)
+    }
+
+}
+
+
+extension Comparable where Self: Weighted {
+
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        lhs.weight < rhs.weight
+    }
+}
+
+
+extension Collection where Element: Weighted {
+
+    public var weight: Element.Weight {
+        reduce(into: .zero) { $0 += $1.weight }
+    }
+}
+
+extension Set: Weighted where Element: Weighted {}
+
+extension Array: Weighted where Element: Weighted {}
