19b7eaaffe47a03e1c2d61a5df25512192485765
Tom Drummond <tom@updtd.com>
2023-07-14 17:18:17 -0700

    Move Graph+Incidence methods to Graph.swift

--- Sources/SwiftPOGL/Graph.swift
+++ Sources/SwiftPOGL/Graph.swift
@@ -42,6 +42,10 @@ public extension Graph {
     var order: Int {
         nodes.count
     }
+}
+
+
+public extension Graph {
 
     /// The degree of a node in a graph or digraph is the number of edges that
     /// are incident on it i.e. that are connected to the node
@@ -52,4 +56,25 @@ public extension Graph {
         edges.count(where: { $0.isIncident(on: node) })
     }
 
+    /// The neighbors of a node are the set of nodes that it is adjacent to (i.e. that are
+    /// connected by an edge).  This always _excludes_ the node provided.
+    ///
+    /// - SeeAlso: ``neighborhood(of:closed:)``
+    func neighbors(of node: NodeType) -> Set<NodeType> {
+        edges.neighbors(of: node)
+    }
+
+    /// The neighborgood of a node is a graph of all the nodes that are adjacent to it
+    /// and the edges that connect them (also known as an induced subgraph). The closed
+    /// neighborhood of a node _includes_ the node & its immediate edges.  The open
+    /// neighborhood of a node _excludes_ the node & its edges.
+    func neighborhood(of node: NodeType, closed: Bool = false) -> Self {
+        if closed {
+            let edges = edges[of: node]
+            let nodes = edges.nodes
+            return Self(nodes: nodes, edges: edges)
+        } else {
+            fatalError()
+        }
+    }
 }
