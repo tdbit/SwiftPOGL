85695ebb3d453bc1653c22c1acba7d34d00c7380
Tom Drummond <tom@updtd.com>
2023-07-27 22:39:47 +0200

    Add GraphFamily enum, generator and tests

---
+++ Sources/SwiftPOGL/GraphFamily.swift
@@ -0,0 +1,210 @@
+//
+//  GraphFamily.swift
+//  SwiftPOGL
+//
+//  Created by Tom Drummond on 7/6/23.
+//
+
+import Foundation
+
+/// `GraphFamily` is an enumeration of simple graph types used for testing.
+public enum GraphFamily {
+
+    /// A graph with one or more nodes and no edges.
+    case null
+
+    /// A graph where each node is connected to the next node
+    /// for a total of `n - 1` edges.
+    /// 
+    /// ```
+    /// e.g. n=5 =>
+    /// x-x-x-x-x
+    /// ```
+    case path
+
+    /// A graph where each node is connected to the next node and the last node
+    /// is connected to the first node for a total of `n` edges.
+    /// ```
+    /// // e.g. n=6 =>
+    /// //   x---x
+    /// //  /     \
+    /// // x       x
+    /// //  \     /
+    /// //   x---x
+    /// ```
+    case cycle
+
+    /// A graph where each node is connected to the center node for a total
+    /// of `n - 1` edges.
+    ///
+    /// ```
+    /// // e.g. n=7 =>
+    /// //  x   x
+    /// //   \ /
+    /// // x--x--x
+    /// //   / \
+    /// //  x   x
+    /// ```
+    case star
+
+    /// A graph where each node is connected to the center node  and to the
+    /// nodes to its left and right for a total of `2 * (n - 1)` edges.
+    ///
+    /// ```
+    /// // e.g. n=7 =>
+    /// //   x---x
+    /// //  / \ / \
+    /// // x---x---x
+    /// //  \ / \ /
+    /// //   x---x
+    /// ```
+    case wheel
+
+    /// A graph of an even number of nodes arranged in two cycles of equal size
+    /// with each node in one cycle connected to its corresponding node in the
+    /// other cycle and each node connected to the next node in its cycle for a
+    /// total of `3 * n / 2` edges.
+    ///
+    /// ```
+    /// // e.g. n=12 =>
+    /// //     x-----x
+    /// //    / \   / \ 
+    /// //   /   x-x   \
+    /// //  /   /   \   \
+    /// // x---x     x---x
+    /// //  \   \   /   /
+    /// //   \   x-x   /
+    /// //    \ /   \ /
+    /// //     x-----x
+    /// ```
+    case prism
+
+    /// A rectangular graph with `m * n` nodes where each node is connected to
+    /// between 2 and 4 neighbors for a total of `2 * m * n - m - n` edges.
+    ///
+    /// ```
+    /// // e.g. m=3, n=5 =>
+    /// // x--x--x--x--x
+    /// // |  |  |  |  |
+    /// // x--x--x--x--x
+    /// // |  |  |  |  |
+    /// // x--x--x--x--x
+    /// ```
+    case mesh(m: Int, n: Int)
+
+    /// A 2-dimensional cycle graph with `m x n` nodes where every node is
+    /// connected to 4 neighbours, with edges wrapping around to create a closed
+    /// loop in both directions for a total of `2 * m * n` edges.
+    /// 
+    /// ```
+    /// // e.g. m=3, n=5 =>
+    /// //   |  |  |  |  |
+    /// // --x--x--x--x--x--
+    /// //   |  |  |  |  |
+    /// // --x--x--x--x--x--
+    /// //   |  |  |  |  |
+    /// // --x--x--x--x--x--
+    /// //   |  |  |  |  |
+    /// ```
+   case torus(m: Int, n: Int)
+
+    /// A graph where each node is connected to every other node for a total of
+    /// `n * (n - 1) / 2` edges in an undirected graph or `n * (n - 1)` edges in
+    /// a directed graph.
+    case complete
+}
+
+
+extension Graph {
+
+    public static func generate(_ family: GraphFamily, nodes: [NodeType]) -> Self {
+        switch family {
+        case .null:
+            return Self(nodes: nodes, edges: [])
+
+        case .path:
+            let edges = nodes[1...].indices.map { (nodes[$0 - 1], nodes[$0]) }
+            return Self(nodes: nodes, edges: edges)
+
+        case .cycle:
+            let edges = nodes.indices.map { (nodes[$0], nodes[($0 + 1) % nodes.count]) }
+            return Self(nodes: nodes, edges: edges)
+
+        case .star:
+            let edges = nodes.indices.map { (nodes[0], nodes[$0]) }
+            return Self(nodes: nodes, edges: Array(edges[1...]))
+
+        case .wheel:
+            let starEdges = nodes.indices.map { (nodes[0], nodes[$0]) }
+            let cycleNodes = Array(nodes[1...])
+            let cycleEdges = cycleNodes.indices.map { (cycleNodes[$0], cycleNodes[($0 + 1) % cycleNodes.count]) }
+            return Self(nodes: nodes, edges: starEdges + cycleEdges)
+
+        case .prism:
+            guard nodes.count > 5 else {
+                fatalError("Prism graphs must have at least 6 nodes.")
+            }
+
+            guard nodes.count % 2 == 0 else {
+                fatalError("Prism graphs must have an even number of nodes.")
+            }
+
+            let m = nodes.count / 2
+            var edges: [(NodeType, NodeType)] = []
+            edges.reserveCapacity(3 * m / 2)
+
+            for i in 0..<m {
+                let j = i + m
+                edges.append((nodes[i], nodes[j]))                  // from node in cycle 1 to node in cycle 2
+                edges.append((nodes[i], nodes[(i + 1) % m]))        // from node in cycle 1 to next node in cycle 1
+                edges.append((nodes[j], nodes[(i + 1) % m + m]))    // from node in cycle 2 to next node in cycle 2
+            }
+            return Self(nodes: nodes, edges: edges)
+
+       case .mesh(let m, let n):
+            guard m * n == nodes.count else {
+               fatalError("There must be exactly m * n nodes.")
+            }
+
+            var edges: [(NodeType, NodeType)] = []
+            for i in 0..<m {
+                for j in 0..<n {
+                    if (j < n - 1) {
+                        edges.append((nodes[i * n + j], nodes[i * n + j + 1]))
+                    }
+                    if (i < m - 1) {
+                        edges.append((nodes[i * n + j], nodes[(i + 1) * n + j]))
+                    }
+                }
+            }
+
+           return Self(nodes: nodes, edges: edges)
+
+        case .torus(let m, let n):
+            guard m * n == nodes.count else {
+               fatalError("There must be exactly m * n nodes.")
+            }
+
+            var edges: [(NodeType, NodeType)] = []
+            for i in 0..<m {
+                for j in 0..<n {
+                    edges.append((nodes[i * n + j], nodes[(i * n + j + 1) % n + i * n]))
+                    edges.append((nodes[i * n + j], nodes[((i + 1) % m * n)  + j]))
+                }
+            }
+
+            return Self(nodes: nodes, edges: edges)
+
+        case .complete:
+            var edges: [(NodeType, NodeType)] = []
+            edges.reserveCapacity(nodes.count * nodes.count)
+
+            for u in nodes {
+                for v in nodes {
+                    edges.append((u,v))
+                }
+            }
+            return Self(nodes: nodes, edges: edges)
+        }
+    }
+}
---
+++ Tests/SwiftPOGLTests/SwiftPOGLFamilyTests.swift
@@ -0,0 +1,178 @@
+//
+//  SwiftPOGLFamilyTests.swift
+//  SwiftPOGL
+//
+//  Created by Tom Drummond on 7/26/23.
+//
+
+import Foundation
+
+import XCTest
+@testable import SwiftPOGL
+
+final class SwiftPOGLFamilyTests: XCTestCase {
+
+    func testNull() {
+        let nodes = Array(1...15)
+        let graph = UndirectedGraph.generate(.null, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, 0, "Null graphs should have no edges.")
+        XCTAssertEqual(graph.nodes.count, 15, "Null graphs should have all given nodes.")
+
+        for node in nodes {
+            XCTAssertEqual(graph.neighbors(of: node), [], "Every node in a null graphs should have no neighbors.")
+        }
+    }
+
+    func testPath() {
+        let nodes = Array(1...15)
+        let graph = UndirectedGraph.generate(.path, nodes: nodes)
+        let digraph = DirectedGraph.generate(.path, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, nodes.count - 1, "Path graphs should have 1 fewer edges than nodes.")
+        XCTAssertEqual(digraph.edges.count, nodes.count - 1, "Path digraphs should have 1 fewer edges than nodes.")
+
+        for node in nodes[1..<nodes.count - 1] {
+            XCTAssertEqual(graph.degree(of: node), 2, "All nodes in a path graph that are not endpoints should have 2 neighbors.")
+            XCTAssertEqual(digraph.inDegree(of: node), 1, "All nodes in a path digraph exluding endpoints should have 1 upstream node.")
+            XCTAssertEqual(digraph.outDegree(of: node), 1, "All nodes in a path digraph excluding endpoints should have 1 downstream node.")
+        }
+
+        XCTAssertEqual(graph.neighbors(of: 1), [2], "Endpoints in a path graph should have 1 neighbor.")
+        XCTAssertEqual(digraph.neighbors(of: 1), [2], "Start point in a path digraph should 1 neighbor.")
+
+        XCTAssertEqual(graph.neighbors(of: 15), [14], "Endpoints in a path graph should have 1 neighbor.")
+        XCTAssertEqual(digraph.neighbors(of: 15), [14], "End point in a path digraph should 1 neighbor.")
+    }
+
+    func testCycle() {
+        let nodes = Array(1...9)
+        let graph = UndirectedGraph.generate(.cycle, nodes: nodes)
+        let digraph = DirectedGraph.generate(.cycle, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, nodes.count, "Cycle graphs should have the same number of edges as nodes.")
+        XCTAssertEqual(digraph.edges.count, nodes.count, "Cycle digraphs should have the same number of edges as nodes.")
+
+        for i in nodes.indices {
+            let index = i + nodes.count // Allow for -1
+            let neighbors = Set([nodes[(index - 1) % nodes.count], nodes[(index + 1) % nodes.count]])
+            XCTAssertEqual(graph.neighbors(of: nodes[i]), neighbors, "Neighbors in a cycle graph should be adjacent nodes.")
+            XCTAssertEqual(digraph.neighbors(of: nodes[i]), neighbors, "Neighbors in a cycle digraph should be adjacent nodes.")
+        }
+    }
+
+    func testStar() {
+        let nodes = Array(1...9)
+        let graph = UndirectedGraph.generate(.star, nodes: nodes)
+        let digraph = DirectedGraph.generate(.star, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, nodes.count - 1, "Star graphs should have 1 fewer edges than nodes.")
+        XCTAssertEqual(digraph.edges.count, nodes.count - 1, "Star digraphs should have 1 fewer edges than nodes.")
+
+        XCTAssertEqual(graph.neighbors(of: 1), Set(nodes[1..<nodes.count]), "The center node in a star graph should have all other nodes as neighbors.")
+        XCTAssertEqual(digraph.neighbors(of: 1), Set(nodes[1..<nodes.count]), "The center node in a star digraph should have all other nodes as neighbors.")
+        
+        for i in nodes.indices.dropFirst() {
+            XCTAssertEqual(graph.neighbors(of: nodes[i]), [1], "All nodes in a star graph excluding the center node should have 1 neighbor.")
+            XCTAssertEqual(digraph.neighbors(of: nodes[i]), [1], "All nodes in a star digraph excluding the center node should have 1 neighbor.")
+        }
+    }
+
+    func testWheel() {
+        let nodes = Array(1...15)
+        let graph = UndirectedGraph.generate(.wheel, nodes: nodes)
+        let digraph = DirectedGraph.generate(.wheel, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, 2 * (nodes.count - 1), "Wheel graphs should have 2 less than 2 times as many edges as nodes.")
+        XCTAssertEqual(digraph.edges.count, 2 * (nodes.count - 1), "Wheel digraphs should have 2 less than 2 times as many edges as nodes.")
+
+        XCTAssertEqual(graph.neighbors(of: 1), Set(nodes[1..<nodes.count]), "The center node in a wheel graph should have all other nodes as neighbors.")
+        XCTAssertEqual(digraph.neighbors(of: 1), Set(nodes[1..<nodes.count]), "The center node in a wheel digraph should have all other nodes as neighbors.")
+
+        XCTAssertEqual(graph.neighbors(of: nodes[1]), [1,3,15], "Neighbors in a wheel graph should be adjacent nodes and the center node.")
+        XCTAssertEqual(digraph.neighbors(of: nodes[1]), [1,3,15], "Neighbors in a wheel digraph should be adjacent nodes and the center node.")
+    }
+
+    func testPrism() { 
+        let nodes = Array(1...8)
+        let graph = UndirectedGraph.generate(.prism, nodes: nodes)
+        let digraph = DirectedGraph.generate(.prism, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, 3 * nodes.count / 2, "Prism graphs should have 1.5x as many edges as nodes.")
+        XCTAssertEqual(digraph.edges.count, 3 * nodes.count / 2, "Prism digraphs should have 1.5x times as many edges as nodes.")
+
+        for node in nodes {
+            XCTAssertEqual(graph.degree(of: node), 3, "All nodes in a prism graph should have 3 neighbors.")
+            XCTAssertEqual(digraph.degree(of: node), 3, "All nodes in a prism digraph should have 3 neighbors.")
+        }
+    }
+
+    func testMesh() {
+        let m = 5
+        let n = 3
+        let nodes = Array(1...m*n)
+        let graph = UndirectedGraph.generate(.mesh(m: m, n: n), nodes: nodes)
+        let digraph = DirectedGraph.generate(.mesh(m: m, n: n), nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, 2 * m * n - m - n, "Mesh graphs should have twice as many edges as nodes less the number of rows and columns.")
+        XCTAssertEqual(digraph.edges.count, 2 * m * n - m - n, "Mesh digraphs should have twice as many edges as nodes less the number of rows and columns.")
+
+        XCTAssertEqual(graph.neighbors(of: 1), [2, n + 1], "Corner nodes in a mesh graph should have 2 neighbors.")
+        XCTAssertEqual(digraph.neighbors(of: 1), [2, n + 1], "Corner nodes in a mesh digraph should have 2 neighbors.")
+
+        XCTAssertEqual(graph.neighbors(of: m * n), [m * n - 1, m * n - n], "Corner nodes in a mesh graph should have 2 neighbors.")
+        XCTAssertEqual(digraph.neighbors(of: m * n), [m * n - 1, m * n - n], "Corner nodes in a mesh digraph should have 2 neighbors.")
+    }
+
+    func testTorus() {
+        let m = 3
+        let n = 5
+        let nodes = Array(1...m*n)
+        let graph = UndirectedGraph.generate(.torus(m: m, n: n), nodes: nodes)
+        let digraph = DirectedGraph.generate(.torus(m: m, n: n), nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, 2 * m * n, "Torus graphs should have 2 * m * n edges.")
+        XCTAssertEqual(digraph.edges.count, 2 * m * n, "Torus digraphs should have 2 * m * n edges.")
+
+        for node in nodes {
+            XCTAssertEqual(graph.degree(of: node), 4, "All nodes in a torus graph should have 4 neighbors.")
+            XCTAssertEqual(digraph.degree(of: node), 4, "All nodes in a torus digraph should have 4 neighbors.")
+        }
+
+        //   |  |  |  |  |
+        // --1--2--3--4--5-
+        //   |  |  |  |  |
+        // --6--7--8--9-10-
+        //   |  |  |  |  |
+        // -11-12-13-14-15-
+        //   |  |  |  |  |
+
+        let results:[(Int, Set<Int>)] =  [
+            (1, [2, 6, 5, 11]),
+            (2, [3, 7, 1, 12]),
+            (8, [3, 9, 13, 7]),
+            (10, [5, 6, 15, 9]),
+            (15, [10, 11, 5, 14]),
+        ]
+
+        for result in results {
+            XCTAssertEqual(graph.neighbors(of: result.0), result.1, "Neighbors should wrap around in a torus graph")
+            XCTAssertEqual(digraph.neighbors(of: result.0), result.1, "Neighbors should wrap around in a torus digraph")
+        }
+    }
+
+    func testComplete() {
+        let nodes = Array(1...20)
+        let graph = UndirectedGraph.generate(.complete, nodes: nodes)
+        let digraph = DirectedGraph.generate(.complete, nodes: nodes)
+
+        XCTAssertEqual(graph.edges.count, nodes.count * (nodes.count - 1) /  2)
+        XCTAssertEqual(digraph.edges.count, nodes.count * (nodes.count - 1))
+
+        for node in nodes {
+            let neighbors = graph.nodes.filter({ $0 != node })
+            XCTAssertEqual(graph.neighbors(of: node), neighbors, "All nodes in a complete graph should be connected to all other nodes.")
+            XCTAssertEqual(digraph.neighbors(of: node), neighbors, "All nodes in a complete digraph should be connected to all other nodes.")
+        }
+    }
+}
